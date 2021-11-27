b44a4fc03e72b0b8036e98e665a0e2535b3e440d
galihtrisna <galihtrisna666@gmail.com>
2021-11-27 16:42:36 +0700

    Add activity Pesanan

---
+++ app/src/main/java/com/stematel/catetin/PesananActivity.kt
@@ -0,0 +1,44 @@
+package com.stematel.catetin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class PesananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesanan)
+
+        val buttonNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        buttonNavigationView.selectedItemId = R.id.pesanan
+
+        buttonNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    startActivity(
+                        Intent(
+                            //applicationContext, PesananActivity::class.java
+                        )
+                    )
+                    finish()
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.akun -> {
+                    startActivity(
+                        Intent(
+                            //applicationContext, AkunActivity::class.java
+                        )
+                    )
+                    finish()
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.pesanan -> return@OnNavigationItemSelectedListener true
+            }
+            false
+        })
+    }
+}
